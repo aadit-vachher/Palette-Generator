4e008d0a36227ac1b4789a5bc1529967784ba29c
Aadit Vachher <vacaadit@gmail.com>
2025-04-18 13:07:53 +0530

    new

--- ColorPallete/src/Components/ColorInput.jsx
+++ ColorPallete/src/Components/ColorInput.jsx
@@ -1,38 +1,6 @@
 
 
 
-// import React, { useState } from 'react';
-// import './ColorInput.css';
-
-// function ColorInput({ onSubmit }) {
-//   const [selectedColor, setSelectedColor] = useState('#3498db');
-
-//   const handleChange = (e) => {
-//     const newColor = e.target.value;
-//     setSelectedColor(newColor);
-//     onSubmit(newColor);
-//   };
-
-//   return (
-//     <div className="color-input-container">
-//       <div className="color-label">
-//         <label htmlFor="colorPicker">Pick a Color:</label>
-//         <input
-//           id="colorPicker"
-//           type="color"
-//           value={selectedColor}
-//           onChange={handleChange}
-//           className="color-picker"
-//         />
-//       </div>
-//     </div>
-//   );
-// }
-
-// export default ColorInput;
-
-
-
 import React, { useState } from 'react';
 import './ColorInput.css';
 
